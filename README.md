b203ded943dca2f6df875f86c734474af0c1cc41
Mirza <mirza.arnaut45@gmail.com>
2023-07-18 02:49:53 +0200

    Change readme, install and setup a gitconfig file

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# dotfiles
-.config files
+# My dotfile setup
+
+## Install
+```shell
+git clone https://github.com/arunoruto/dotfiles ~/.dotfiles
+cd ~/.dotfiles
+./install.sh
+```
+
+# Inspirations
+Some videos and articles from where the configs are based on.
+## Nvim
+[![Turn VIM into a full featured IDE with only one command](https://img.youtube.com/vi/Mtgo-nP_r8Y/0.jpg)](https://www.youtube.com/watch?v=Mtgo-nP_r8Y)
+## Tmux
+[![Tmux has forever changed the way I write code.](https://img.youtube.com/vi/DzNmUNvnB04/0.jpg)](https://www.youtube.com/watch?v=DzNmUNvnB04)
+
+# TODO
+Create [ansible](https://www.ansible.com/) playbooks for installing all packages and setting up the configs!
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Install tpm for tmux
 mkdir -p $HOME/.config/tmux/plugins
-git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm $HOME/.config/ tmux/plugins/tpm
 ln -s $PWD/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
 
 # Install nvchad
@@ -11,6 +11,13 @@ git clone https://github.com/NvChad/NvChad $HOME/.config/nvim --depth 1
 ln -s $PWD/nvim/lua/custom $HOME/.config/nvim/lua/custom
 
 # Install oh-my-zsh
-mkdir -p $HOME/.config/zsh
 git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.config/zsh
+mv ~/.zshrc ~/.zshrc.bak
 ln -s $PWD/.zshrc $HOME/.zshrc
+
+## Install zsh plugins
+ZSH_CUSTOM=$HOME/.config/zsh/custom
+mkdir -p $ZSH_CUSTOM
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
